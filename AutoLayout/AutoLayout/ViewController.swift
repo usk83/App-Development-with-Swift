6722ee215ed1ced6c56292537442eb47856d83ed
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2020-04-17 00:33:44 -0700

    Change indent size 2 spaces -> 4 spaces

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -9,115 +9,115 @@
 import UIKit
 
 class ViewController: UIViewController {
-  let mainView: UIView = {
-    let main = UIView()
-    // important when setting contraints programmatically
-    main.translatesAutoresizingMaskIntoConstraints = false
-    main.backgroundColor = .green
-    return main
-  }()
+    let mainView: UIView = {
+        let main = UIView()
+        // important when setting contraints programmatically
+        main.translatesAutoresizingMaskIntoConstraints = false
+        main.backgroundColor = .green
+        return main
+    }()
 
-  let squareButton: UIButton = {
-    let butt = UIButton(type: .system)
-    butt.setTitle("Square", for: .normal)
-    butt.translatesAutoresizingMaskIntoConstraints = false
-    butt.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-    butt.addTarget(self, action: #selector(squareTapped), for: .touchUpInside)
-    return butt
-  }()
+    let squareButton: UIButton = {
+        let butt = UIButton(type: .system)
+        butt.setTitle("Square", for: .normal)
+        butt.translatesAutoresizingMaskIntoConstraints = false
+        butt.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        butt.addTarget(self, action: #selector(squareTapped), for: .touchUpInside)
+        return butt
+    }()
 
-  let portraitButton: UIButton = {
-    let butt = UIButton(type: .system)
-    butt.setTitle("Portrait", for: .normal)
-    butt.translatesAutoresizingMaskIntoConstraints = false
-    butt.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-    butt.addTarget(self, action: #selector(portraitTapped), for: .touchUpInside)
-    return butt
-  }()
+    let portraitButton: UIButton = {
+        let butt = UIButton(type: .system)
+        butt.setTitle("Portrait", for: .normal)
+        butt.translatesAutoresizingMaskIntoConstraints = false
+        butt.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        butt.addTarget(self, action: #selector(portraitTapped), for: .touchUpInside)
+        return butt
+    }()
 
-  let landScapeButton: UIButton = {
-    let butt = UIButton(type: .system)
-    butt.setTitle("Landscape", for: .normal)
-    butt.translatesAutoresizingMaskIntoConstraints = false
-    butt.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-    butt.addTarget(self, action: #selector(landscapeTapped), for: .touchUpInside)
-    return butt
-  }()
+    let landScapeButton: UIButton = {
+        let butt = UIButton(type: .system)
+        butt.setTitle("Landscape", for: .normal)
+        butt.translatesAutoresizingMaskIntoConstraints = false
+        butt.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        butt.addTarget(self, action: #selector(landscapeTapped), for: .touchUpInside)
+        return butt
+    }()
 
-  var widthAnchor: NSLayoutConstraint?
-  var heightAnchor: NSLayoutConstraint?
+    var widthAnchor: NSLayoutConstraint?
+    var heightAnchor: NSLayoutConstraint?
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = .white
-    view.addSubview(mainView)
-    setupLayout()
-  }
-
-  fileprivate func setupLayout() {
-    mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    mainView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-    widthAnchor = mainView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7, constant: 0)
-    widthAnchor?.isActive = true
-
-    heightAnchor = mainView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7, constant: 0)
-    heightAnchor?.isActive = true
-
-    let buttStackView = UIStackView(arrangedSubviews: [
-      squareButton, portraitButton, landScapeButton])
-    buttStackView.translatesAutoresizingMaskIntoConstraints = false
-    buttStackView.axis = .horizontal
-    buttStackView.alignment = .center
-    buttStackView.distribution = .fillEqually
-
-    view.addSubview(buttStackView)
-    NSLayoutConstraint.activate([
-      buttStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
-      buttStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      buttStackView.heightAnchor.constraint(equalToConstant: 50),
-      buttStackView.widthAnchor.constraint(equalTo: view.widthAnchor)
-      ])
-  }
-
-  @objc fileprivate func squareTapped() {
-    view.layoutIfNeeded()
-    UIView.animate(withDuration: 2.0) {
-      self.widthAnchor?.isActive = false
-      self.widthAnchor? = self.mainView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.9)
-      self.widthAnchor?.isActive = true
-
-      self.heightAnchor?.isActive = false
-      self.heightAnchor? = self.mainView.heightAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.9)
-      self.heightAnchor?.isActive = true
-      self.view.layoutIfNeeded()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(mainView)
+        setupLayout()
     }
-  }
 
-  @objc fileprivate func portraitTapped() {
-    view.layoutIfNeeded()
-    UIView.animate(withDuration: 2.0) {
-      self.widthAnchor?.isActive = false
-      self.widthAnchor? = self.mainView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.7)
-      self.widthAnchor?.isActive = true
+    fileprivate func setupLayout() {
+        mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mainView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        widthAnchor = mainView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7, constant: 0)
+        widthAnchor?.isActive = true
 
-      self.heightAnchor?.isActive = false
-      self.heightAnchor? = self.mainView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.7)
-      self.heightAnchor?.isActive = true
-      self.view.layoutIfNeeded()
+        heightAnchor = mainView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.7, constant: 0)
+        heightAnchor?.isActive = true
+
+        let buttStackView = UIStackView(arrangedSubviews: [
+            squareButton, portraitButton, landScapeButton])
+        buttStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttStackView.axis = .horizontal
+        buttStackView.alignment = .center
+        buttStackView.distribution = .fillEqually
+
+        view.addSubview(buttStackView)
+        NSLayoutConstraint.activate([
+            buttStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            buttStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttStackView.heightAnchor.constraint(equalToConstant: 50),
+            buttStackView.widthAnchor.constraint(equalTo: view.widthAnchor)
+            ])
     }
-  }
 
-  @objc fileprivate func landscapeTapped() {
-    view.layoutIfNeeded()
-    UIView.animate(withDuration: 2.0) {
-      self.widthAnchor?.isActive = false
-      self.widthAnchor? = self.mainView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.95)
-      self.widthAnchor?.isActive = true
+    @objc fileprivate func squareTapped() {
+        view.layoutIfNeeded()
+        UIView.animate(withDuration: 2.0) {
+            self.widthAnchor?.isActive = false
+            self.widthAnchor? = self.mainView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.9)
+            self.widthAnchor?.isActive = true
 
-      self.heightAnchor?.isActive = false
-      self.heightAnchor? = self.mainView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.4)
-      self.heightAnchor?.isActive = true
-      self.view.layoutIfNeeded()
+            self.heightAnchor?.isActive = false
+            self.heightAnchor? = self.mainView.heightAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.9)
+            self.heightAnchor?.isActive = true
+            self.view.layoutIfNeeded()
+        }
     }
-  }
+
+    @objc fileprivate func portraitTapped() {
+        view.layoutIfNeeded()
+        UIView.animate(withDuration: 2.0) {
+            self.widthAnchor?.isActive = false
+            self.widthAnchor? = self.mainView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.7)
+            self.widthAnchor?.isActive = true
+
+            self.heightAnchor?.isActive = false
+            self.heightAnchor? = self.mainView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.7)
+            self.heightAnchor?.isActive = true
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc fileprivate func landscapeTapped() {
+        view.layoutIfNeeded()
+        UIView.animate(withDuration: 2.0) {
+            self.widthAnchor?.isActive = false
+            self.widthAnchor? = self.mainView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.95)
+            self.widthAnchor?.isActive = true
+
+            self.heightAnchor?.isActive = false
+            self.heightAnchor? = self.mainView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.4)
+            self.heightAnchor?.isActive = true
+            self.view.layoutIfNeeded()
+        }
+    }
 }
